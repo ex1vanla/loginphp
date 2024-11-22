a1b990b884e25e723f5ef3a020c098c7a4f98ad2
ex1vanla <vanla@trithucmoi.co>
2024-11-22 16:11:00 +0900

    check connect db

--- login.php
+++ login.php
@@ -32,6 +32,8 @@ $errors = array();
 
 // Kết nối tới cơ sở dữ liệu MySQL
 $db = mysqli_connect('mysql-sever.mysql.database.azure.com', 'vanla', 'Exone123@', 'test2');
+var_dump($db);
+
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
 }
